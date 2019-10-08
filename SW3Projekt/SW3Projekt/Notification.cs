bdc172ad4820a1387a5948b7691feb49a4e7dfcd
AsBeeb <asbeeb@gmail.com>
2019-10-08 10:25:21 +0200

    Added last classes

---
+++ SW3Projekt/SW3Projekt/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW3Projekt
+{
+    class Notification
+    {
+        public int DatabaseID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ SW3Projekt/SW3Projekt/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW3Projekt
+{
+    class User
+    {
+        //User information
+        public int DatabaseID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        //User settings
+        public int SixtyDayThreshold { get; set; } = 50;
+
+    }
+}
