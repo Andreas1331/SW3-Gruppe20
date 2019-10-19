35a67d5970d794a5ba9e8819bb8fcb2a3b38dc41
-Andreas <andreaschristensen2010@live.dk>
2019-10-19 15:50:13 +0200

    Implemented basic list for employees page
    - Will show all employees in a grid
    - Can double click and employee

--- SW3Projekt/SW3Projekt/ViewModels/EmployeesViewModel.cs
+++ SW3Projekt/SW3Projekt/ViewModels/EmployeesViewModel.cs
@@ -1,13 +1,34 @@
 ﻿using Caliburn.Micro;
+using SW3Projekt.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SW3Projekt.ViewModels
 {
     public class EmployeesViewModel : Screen
     {
+        public BindableCollection<Employee> Employees { get; set; }
+        public Employee SelectedEmployee { get; set; }
+
+        public EmployeesViewModel()
+        {
+            Employees = new BindableCollection<Employee>(GetEmployees());
+        }
+
+        public void EmployeeDoubleClicked()
+        {
+            Console.WriteLine("NAME: " + SelectedEmployee?.Firstname);
+        }
+
+        private List<Employee> GetEmployees()
+        {
+            // TODO: Query the database for the employees instead of generating a testing list
+            return new List<Employee>()
+                    {
+                        new Employee(){Firstname = "Andreas", Surname = "Christensen"},
+                        new Employee(){Firstname = "Andreas", Surname = "Andersen"},
+                        new Employee(){Firstname = "Michael", Surname = "Michaelsen"},
+                    };
+        }
     }
 }
