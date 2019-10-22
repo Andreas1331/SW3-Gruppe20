28b1ff9f8fc5b25f8b3edbcdcf14e3ce3848426a
AsBeeb <asbeeb@gmail.com>
2019-10-22 14:10:52 +0200

    AddAgreement added

---
+++ SW3Projekt/SW3Projekt/ViewModels/AddAgreementViewModel.cs
@@ -0,0 +1,31 @@
+﻿using SW3Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW3Projekt.ViewModels
+{
+    public class AddAgreementViewModel
+    {
+        public void CreateNewAgreement()
+        {
+            //CollectiveAgreement ca = new CollectiveAgreement()
+            //{
+            //    Name =,
+            //    Start,
+            //    End,
+            //    Rates,
+            //    IsActive
+            //}
+
+            //using (var ctx = new SW3Projekt.DatabaseDir.Database())
+            //{
+            //    ctx.CollectiveAgreements.Add(ca);
+            //    ctx.SaveChanges();
+            //}
+
+        }
+    }
+}
