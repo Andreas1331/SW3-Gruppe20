35706f3fecb622cd14d5f9766baad85b123a9e61
AsBeeb <asbeeb@gmail.com>
2019-10-23 11:08:50 +0200

    AddAgreement
    Ability to add rates.
    Ability to use buttons to remove/add checkmarks
    Missing ability to remove rates.

--- SW3Projekt/SW3Projekt/ViewModels/AddAgreementViewModel.cs
+++ SW3Projekt/SW3Projekt/ViewModels/AddAgreementViewModel.cs
@@ -1,14 +1,31 @@
-﻿using SW3Projekt.Models;
+﻿using Caliburn.Micro;
+using SW3Projekt.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SW3Projekt.ViewModels
 {
-    public class AddAgreementViewModel
+    public class AddAgreementViewModel : Conductor<object>
     {
+        public ObservableCollection<object> RateEntries { get; set; } = new ObservableCollection<object>();
+        
+        public void RemoveRatesFromCA(ObservableCollection<AddRateViewModel> collection, AddRateViewModel instance)
+        {
+            collection.Remove(collection.Where(i => i.Id == instance.Id).Single());
+        }
+
+        public int rateId = 0;
+        public void BtnAddRatesToCA()
+        {
+            RateEntries.Add(new AddRateViewModel() {Id = rateId++ });
+        }
+
+
+
         public void CreateNewAgreement()
         {
             //CollectiveAgreement ca = new CollectiveAgreement()
