0f43ffa47dc09f952709d51692e55696bb9246f4
Shpend Gjela <45763057+ShpendG@users.noreply.github.com>
2019-10-25 12:54:27 +0200

    Update EmployeesViewModel.cs

--- SW3Projekt/SW3Projekt/ViewModels/EmployeesViewModel.cs
+++ SW3Projekt/SW3Projekt/ViewModels/EmployeesViewModel.cs
@@ -10,118 +10,114 @@ using System.Windows.Controls;
 
 namespace SW3Projekt.ViewModels
 {
-    public class EmployeesViewModel : Conductor<object>
+  public class EmployeesViewModel : Conductor<object>
+  {
+    // List to constantly keep track of all the employees
+    private List<Employee> AllEmployees { get; set; }
+    // Collection used to determin which employees are currently being shown on the datagrid
+    private BindableCollection<Employee> _employeeCollection;
+    public BindableCollection<Employee> EmployeeCollection {
+      get { return _employeeCollection; }
+      set {
+        _employeeCollection = value;
+        NotifyOfPropertyChange(() => EmployeeCollection);
+      }
+    }
+    // Reference to the employee currently selected by the user on the datagrid
+    public Employee SelectedEmployee { get; set; }
+
+    private Employee _newEmployee;
+    public Employee NewEmployee {
+      get {
+        return _newEmployee;
+      }
+      set {
+        _newEmployee = value;
+        NotifyOfPropertyChange<Employee>(() => NewEmployee);
+      }
+    }
+    // This datetime is used to display the current day when,
+    // adding a new employee.
+    public DateTime DaysDate { get; } = DateTime.Now;
+
+    private string _searchEmployeeText = "";
+    public string SearchEmployeeText {
+      get {
+        return _searchEmployeeText;
+      }
+      set {
+        _searchEmployeeText = value;
+        NotifyOfPropertyChange(() => SearchEmployeeText);
+        Task.Run(async () => await SearchForEmployeeAsync(SearchEmployeeText));
+      }
+    }
+
+    public EmployeesViewModel()
     {
-        // List to constantly keep track of all the employees
-        private List<Employee> AllEmployees { get; set; }
-        // Collection used to determin which employees are currently being shown on the datagrid
-        private BindableCollection<Employee> _employeeCollection;
-        public BindableCollection<Employee> EmployeeCollection
+      NewEmployee = new Employee();
+      Task.Run(async () =>
+      {
+        AllEmployees = await GetEmployeesAsync();
+        EmployeeCollection = new BindableCollection<Employee>(AllEmployees);
+      });
+    }
+
+    public void EmployeeDoubleClicked()
+    {
+      Console.WriteLine("NAME: " + SelectedEmployee?.Firstname);
+      ActivateItem(new EmployeeProfileViewModel(SelectedEmployee));
+    }
+
+    public bool CanBtnAddNewEmployee()
+    {
+      return false;
+    }
+    public async void BtnAddNewEmployee()
+    {
+      using (var ctx = new Database())
+      {
+        ctx.Employees.Add(NewEmployee);
+        await ctx.SaveChangesAsync();
+
+        AllEmployees = await GetEmployeesAsync();
+        EmployeeCollection = new BindableCollection<Employee>(AllEmployees);
+      }
+    }
+
+    // TODO: Move this searching logic to another place perhaps?? Maybe
+    public async Task SearchForEmployeeAsync(string criteria)
+    {
+      // First determine if the user is searching by employee ID (int) or name (string)
+      int employeeID;
+      if (int.TryParse(criteria, out employeeID))
+      {
+        // Searched by ID
+        EmployeeCollection = await Task.Run(() => new BindableCollection<Employee>(AllEmployees.Where(x => x.Id.ToString().Contains(criteria)).ToList()));
+      }
+      else
+      {
+        // Searched by name
+        // Check if the user cleared the field, and then just display everyone once more
+        if (String.IsNullOrEmpty(criteria))
         {
-            get { return _employeeCollection; }
-            set
-            {
-                _employeeCollection = value;
-                NotifyOfPropertyChange(() => EmployeeCollection);
-            }
+          EmployeeCollection = new BindableCollection<Employee>(AllEmployees);
         }
-        // Reference to the employee currently selected by the user on the datagrid
-        public Employee SelectedEmployee { get; set; }
-
-        private Employee _newEmployee;
-        public Employee NewEmployee
+        // Otherwise attempt to find employees based on the search value
+        else
         {
-            get
-            {
-                return _newEmployee;
-            }
-            set
-            {
-                _newEmployee = value;
-                NotifyOfPropertyChange<Employee>(() => NewEmployee);
-            }
+          EmployeeCollection = await Task.Run(() => new BindableCollection<Employee>(AllEmployees.Where(x => x.Fullname.ToLower().Contains(criteria.ToLower())).ToList()));
         }
-        // This datetime is used to display the current day when,
-        // adding a new employee.
-        public DateTime DaysDate { get; } = DateTime.Now;
+      }
+    }
 
-        private string _searchEmployeeText = "";
-        public string SearchEmployeeText
-        {
-            get
-            {
-                return _searchEmployeeText;
-            }
-            set
-            {
-                _searchEmployeeText = value;
-                NotifyOfPropertyChange(() => SearchEmployeeText);
-                Task.Run(async () => await SearchForEmployeeAsync(SearchEmployeeText));
-            }
-        }
-
-        public EmployeesViewModel()
-        {
-            NewEmployee = new Employee();
-            Task.Run(async () =>
-            {
-                AllEmployees = await GetEmployeesAsync();
-                EmployeeCollection = new BindableCollection<Employee>(AllEmployees);
-            });
-        }
-
-        public void EmployeeDoubleClicked()
-        {
-            Console.WriteLine("NAME: " + SelectedEmployee?.Firstname);
-            ActivateItem(new EmployeeProfileViewModel(SelectedEmployee));
-        }
-
-        public async void BtnAddNewEmployee()
-        {
-            using (var ctx = new Database())
-            {
-                ctx.Employees.Add(NewEmployee);
-                await ctx.SaveChangesAsync();
-
-                AllEmployees = await GetEmployeesAsync();
-                EmployeeCollection = new BindableCollection<Employee>(AllEmployees);
-            }
-        }
-
-        // TODO: Move this searching logic to another place perhaps?? Maybe
-        public async Task SearchForEmployeeAsync(string criteria)
-        {
-            // First determine if the user is searching by employee ID (int) or name (string)
-            int employeeID;
-            if (int.TryParse(criteria, out employeeID))
-            {
-                // Searched by ID
-                EmployeeCollection = await Task.Run(() => new BindableCollection<Employee>(AllEmployees.Where(x => x.Id.ToString().Contains(criteria)).ToList()));
-            }
-            else
-            {
-                // Searched by name
-                // Check if the user cleared the field, and then just display everyone once more
-                if (String.IsNullOrEmpty(criteria))
-                {
-                    EmployeeCollection = new BindableCollection<Employee>(AllEmployees);
-                }
-                // Otherwise attempt to find employees based on the search value
-                else
-                {
-                    EmployeeCollection = await Task.Run(() => new BindableCollection<Employee>(AllEmployees.Where(x => x.Fullname.ToLower().Contains(criteria.ToLower())).ToList()));
-                }
-            }
-        }
-
-        private async Task<List<Employee>> GetEmployeesAsync()
-        {
-            using (var ctx = new Database())
-            {
-                List<Employee> employees = await Task.Run(() => ctx.Employees.ToList());
-                return employees;
-            }
-            return new List<Employee>()
+    private async Task<List<Employee>> GetEmployeesAsync()
+    {
+      using (var ctx = new Database())
+      {
+        List<Employee> employees = await Task.Run(() => ctx.Employees.ToList());
+        return employees;
+      }
+      return new List<Employee>()
                     {
                         new Employee(){Firstname = "Andreas", Surname = "Christensen", Id = 2313},
                         new Employee(){Firstname = "Andreas", Surname = "Andersen", Id = 513},
@@ -131,6 +127,6 @@ namespace SW3Projekt.ViewModels
                         new Employee(){Firstname = "Filip", Surname = "Filipsen", Id = 930},
                         new Employee(){Firstname = "Emil", Surname = "Emilsen", Id = 930}
             };
-        }
     }
+  }
 }
