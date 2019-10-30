6964d22a08dc1fa21f2d428e56918ac014d89ed0
mmoesm <42961608+mmoesm@users.noreply.github.com>
2019-10-30 10:03:51 +0100

    Autofyld af timer ved TimesheetEntry entry og nogle default-værdier sat.

--- SW3Projekt/SW3Projekt/ViewModels/TimesheetEntryViewModel.cs
+++ SW3Projekt/SW3Projekt/ViewModels/TimesheetEntryViewModel.cs
@@ -5,17 +5,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using Caliburn.Micro;
+
 
 namespace SW3Projekt.ViewModels
 {
-    public class TimesheetEntryViewModel
+    public class TimesheetEntryViewModel : Screen
     {
         public TimesheetEntry TimesheetEntry { get; set; }
 
         public TimesheetTemplateViewModel TSTemplateModel { get; set; }
 
         public Timesheet Timesheet { get; set; }
-        public string HoursTextBox { get; set; }
+
+        private string _hourstextboxstring { get; set; }
+        public string HoursTextBox
+        {
+            get
+            {
+                return _hourstextboxstring;
+            }
+            set
+            {
+                _hourstextboxstring = value;
+                NotifyOfPropertyChange(() => HoursTextBox);
+            }
+        }
+
+        public int StartTimeBox
+        {
+            get
+            {
+                return TimesheetEntry.StartTime;
+            }
+            set
+            {
+                TimesheetEntry.StartTime = value;
+                NotifyOfPropertyChange(() => StartTimeBox);
+                UpdateHoursTextbox();
+            }
+        }
+
+        public int EndTimeBox
+        {
+            get
+            {
+                return TimesheetEntry.EndTime;
+            }
+            set
+            {
+                TimesheetEntry.EndTime = value;
+                NotifyOfPropertyChange(() => EndTimeBox);
+                UpdateHoursTextbox();
+            }
+        }
+        public float BreakTimeBox
+        {
+            get
+            {
+                return TimesheetEntry.BreakTime;
+            }
+            set
+            {
+                TimesheetEntry.BreakTime = value;
+                NotifyOfPropertyChange(() => BreakTimeBox);
+                UpdateHoursTextbox();
+            }
+        }
+
 
 
         public TimesheetEntryViewModel(TimesheetTemplateViewModel timesheetViewModel)
@@ -23,12 +80,25 @@ namespace SW3Projekt.ViewModels
             TimesheetEntry = new TimesheetEntry();
             TSTemplateModel = timesheetViewModel;
             TimesheetEntry.timesheet = TSTemplateModel.Timesheet;
+            UpdateHoursTextbox();
         }
 
         public void BtnRemoveEntry()
         {
             TSTemplateModel.RemoveEntry(this);
         }
+
+        private void UpdateHoursTextbox()
+        {
+            //the calculation for hours:
+            float numberOfWholeHours = (float)(Math.Floor((double)EndTimeBox / 100) - Math.Ceiling((double)StartTimeBox / 100));
+
+            //the  calculations for minutes:
+            float numberOfMinutes = (60 - (StartTimeBox % 100 == 0 ? 60 : StartTimeBox % 100) + EndTimeBox % 100) * (5 / 3) / (float)100;
+
+            HoursTextBox = (numberOfMinutes + numberOfWholeHours - BreakTimeBox).ToString();
+        }
+
 
     }
 }
