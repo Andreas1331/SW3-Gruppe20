ad6a513bb3f87f074c0b12300f1f1090a1b87f59
AsBeeb <asbeeb@gmail.com>
2019-11-01 07:45:16 +0100

    Small Sheisse

---
+++ SW3Projekt/SW3Projekt/ViewModels/YearlyOverViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using SW3Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW3Projekt.ViewModels
+{
+    public class YearlyOverViewModel : Screen
+    {
+        //Properties
+        private List<Employee> AllEmployees { get; set; }
+
+
+    }
+}
