4a869946adfa8ebd086c2b39ab3ca67d7aa76d17
Shpend Gjela <45763057+ShpendG@users.noreply.github.com>
2019-11-06 14:23:14 +0100

    Checks for max length in workplace entry

--- SW3Projekt/SW3Projekt/Models/Workplace.cs
+++ SW3Projekt/SW3Projekt/Models/Workplace.cs
@@ -11,9 +11,42 @@ namespace SW3Projekt.Models
     public class Workplace
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Abbreviation { get; set; }
-        public string Address { get; set; }
+
+        private string _name;
+        public string Name {
+            get { return _name; }
+            set { if (value.Length > 50) {
+                    _name = value.Substring(0, 50);
+                    return;
+                }
+                _name = value; }
+            }
+
+        private string _abbreviation;
+        public string Abbreviation {
+            get { return _abbreviation; }
+            set {
+                if (value.Length > 50)
+                {
+                    _abbreviation = value.Substring(0, 50);
+                    return;
+                }
+                _abbreviation = value;
+            }
+        }
+
+        private string _address;
+        public string Address {
+            get { return _address; }
+            set {
+                if (value.Length > 50)
+                {
+                    _address = value.Substring(0, 50);
+                    return;
+                }
+                _address = value;
+            }
+        }
         public bool Archived { get; set; }
         public double MaxPayout { get; set; }
 
