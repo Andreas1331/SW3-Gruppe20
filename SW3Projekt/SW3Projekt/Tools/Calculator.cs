5ee2c26f54f83b36f5344a3fc68e804b37b23a59
mmoesm <42961608+mmoesm@users.noreply.github.com>
2019-11-21 12:25:32 +0100

    Normalrate bliver brugt ved Arbejde + Forskudttid nu.

--- SW3Projekt/SW3Projekt/Tools/Calculator.cs
+++ SW3Projekt/SW3Projekt/Tools/Calculator.cs
@@ -47,7 +47,7 @@ namespace SW3Projekt.Tools
         private static void IsRateApplicable(TimesheetEntry entry, Rate rate)
         {
             // Check for match between rate type and selected item along with a days check.
-            if (entry.SelectedTypeComboBoxItem == rate.Type && DaysApply(rate.DaysPeriod,(int)entry.Date.DayOfWeek))
+            if (TypesCompatible(entry, rate) && DaysApply(rate.DaysPeriod,(int)entry.Date.DayOfWeek))
             {
                 // Hourly or daily rate check.
                 if (rate.StartTime != rate.EndTime)
@@ -71,6 +71,12 @@ namespace SW3Projekt.Tools
         {
             return (daysPeriod & ((Days)Math.Pow(2, entryDay))) > 0;
         }
+
+        private static bool TypesCompatible(TimesheetEntry entry, Rate rate)
+        {
+            return entry.SelectedTypeComboBoxItem == rate.Type || (entry.SelectedTypeComboBoxItem == "Forskudttid" && rate.Name == "Normal");
+        }
+
 
         private static void CheckAndApplyHourlyRate(TimesheetEntry entry, Rate rate)
         {
